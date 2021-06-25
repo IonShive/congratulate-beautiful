0d95aa1ede2909bcca68ed612ce30169da583d41
IonShive <vlad.balaban2020@gmail.com>
2021-06-25 14:57:59 +0300

    added portfolio

--- app/js/main.js
+++ app/js/main.js
@@ -29,6 +29,20 @@ $(function () {
 		autoplaySpeed: 3000,
 	});
 
+	(function () {
+		var s = document['createElement']('script');
+		s.type = 'text/javascript';
+		s.async = true;
+		s.charset = 'utf-8';
+		s.src = '//cleversite.ru/cleversite/widget_new.php?supercode=1&referer_main=' + encodeURIComponent(document.referrer) + '&clid=104206yQbai&siteNew=134551';
+		var ss = document['getElementsByTagName']('script')[0];
+		if (ss) {
+			ss.parentNode.insertBefore(s, ss);
+		} else {
+			document.documentElement.firstChild.appendChild(s);
+		};
+	})();
+
 });
 
 
