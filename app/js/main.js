36c68ab8690bdec03e48f8bafc3e2ab72a2b15d0
IonShive <vlad.balaban2020@gmail.com>
2021-07-01 13:56:31 +0300

    added fix contacts

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,13 @@
 $(function () {
 
+	window.onload = function () {
+		document.body.classList.add('loaded_hiding');
+		window.setTimeout(function () {
+			document.body.classList.add('loaded');
+			document.body.classList.remove('loaded_hiding');
+		}, 500);
+	}
+
 	$('.header__btn').on('click', function () {
 		$('.header__btn').toggleClass('header__btn--active');
 		$('.header__link').toggleClass('header__link--active');
@@ -51,7 +59,52 @@ $(function () {
 		}
 	});
 
+	var errorTxt = 'Ошибка отправки';
+	jQuery("#sendform").validate({
+		submitHandler: function (form) {
+			var form = document.forms.sendform,
+				formData = new FormData(form),
+				xhr = new XMLHttpRequest();
+
+			xhr.open("POST", "send.php");
+
+			xhr.onreadystatechange = function () {
+				if (xhr.readyState == 4) {
+					if (xhr.status == 200) {
+						document.getElementById('sendform').reset();
+						var element = document.getElementById("contacts__file-return");
+						element.classList.add("contacts__file-return--passive");
+						$('.contacts__pop-up').addClass('contacts__pop-up--active');
+					}
+				}
+			};
+			xhr.send(formData);
+		}
+	});
+
+	$('.contacts__file').on('click', function () {
+		$('.contacts__file-return').removeClass('contacts__file-return--passive');
+	});
+
+	$('.contacts__close').on('click', function () {
+		$('.contacts__pop-up').removeClass('contacts__pop-up--active');
+	});
+
+	var fileInput = document.querySelector(".contacts__file"),
+		button = document.querySelector(".contacts__file-trigger"),
+		the_return = document.querySelector(".contacts__file-return");
+
+	button.addEventListener("keydown", function (event) {
+		if (event.keyCode == 13 || event.keyCode == 32) {
+			fileInput.focus();
+		}
+	});
+	button.addEventListener("click", function (event) {
+		fileInput.focus();
+		return false;
+	});
+	fileInput.addEventListener("change", function (event) {
+		the_return.innerHTML = this.value;
+	});
+
 });
-
-
-
