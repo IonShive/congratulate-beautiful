c84b50937ee450040a2826bf807ff83a9ae88bcc
IonShive <vlad.balaban2020@gmail.com>
2021-07-12 12:17:13 +0300

    added global fix

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,6 @@
 $(function () {
 
 	// HEADER //
-	window.onload = function () {
-		document.body.classList.add('loaded_hiding');
-		window.setTimeout(function () {
-			document.body.classList.add('loaded');
-			document.body.classList.remove('loaded_hiding');
-		}, 500);
-	}
-
 	$('.header__btn').on('click', function () {
 		$('.header__toggle-wrapper').toggleClass('header__toggle-wrapper--active');
 	});
@@ -26,9 +18,8 @@ $(function () {
 		header.classList.toggle('scrolling-active', windowPosition);
 	})
 
-	// CENTRAL //
 	$(window).scroll(function () {
-		$('.central__link-icon').toggleClass('central__link-icon--active', $(this).scrollTop() > 0);
+		$('.header__link-viber').toggleClass('header__link-viber--active', $(this).scrollTop() > 0);
 	});
 
 	// SLIDER //
@@ -82,55 +73,6 @@ $(function () {
 		animation: {
 			duration: 500,
 		}
-	});
-
-	// FORM //
-	var errorTxt = 'Ошибка отправки';
-	jQuery("#sendform").validate({
-		submitHandler: function (form) {
-			var form = document.forms.sendform,
-				formData = new FormData(form),
-				xhr = new XMLHttpRequest();
-
-			xhr.open("POST", "send.php");
-
-			xhr.onreadystatechange = function () {
-				if (xhr.readyState == 4) {
-					if (xhr.status == 200) {
-						document.getElementById('sendform').reset();
-						var element = document.getElementById("contacts__file-return");
-						element.classList.add("contacts__file-return--passive");
-						$('.contacts__pop-up').addClass('contacts__pop-up--active');
-					}
-				}
-			};
-			xhr.send(formData);
-		}
-	});
-
-	$('.contacts__file').on('click', function () {
-		$('.contacts__file-return').removeClass('contacts__file-return--passive');
-	});
-
-	$('.contacts__close').on('click', function () {
-		$('.contacts__pop-up').removeClass('contacts__pop-up--active');
-	});
-
-	var fileInput = document.querySelector(".contacts__file"),
-		button = document.querySelector(".contacts__file-trigger"),
-		the_return = document.querySelector(".contacts__file-return");
-
-	button.addEventListener("keydown", function (event) {
-		if (event.keyCode == 13 || event.keyCode == 32) {
-			fileInput.focus();
-		}
-	});
-	button.addEventListener("click", function (event) {
-		fileInput.focus();
-		return false;
-	});
-	fileInput.addEventListener("change", function (event) {
-		the_return.innerHTML = this.value;
 	});
 
 });
